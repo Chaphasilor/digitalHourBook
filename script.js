ecce82823121074485b49fa44830d186fa7f066e
Chaphasilor <ppp.u@web.de>
2019-01-21 19:48:28 +0100

    minor improvements
    - added more promises for error handling and data retrieval
    - implemented display of overtime
    - overtime is now calculated for the current day

--- script.js
+++ script.js
@@ -13,33 +13,35 @@ function newEntry() {
 var db;
 
 function initDB() {
-  
-  var request = window.indexedDB.open("hourBook", 2);
-  
-  request.onsuccess = function(event) {
+  return new Promise(function(resolve, reject) {
     
-    console.log("DB opened succesfully!");
+    var request = window.indexedDB.open("hourBook", 2);
     
-    db = event.target.result;
-    
-    db.onerror = function(event) {
-      alert("Database error: " + event.target.errorCode);
+    request.onsuccess = function(event) {
+            
+      db = event.target.result;
+      
+      db.onerror = function(event) {
+        reject("Database error: " + event.target.errorCode);
+      }
+      
+      resolve("DB opened successfully!");
+      
     }
     
-  }
+    request.onerror = function() {
+      reject("Couldn't open DB!");
+    }
+    
+    request.onupgradeneeded = function (event) {
   
-  request.onerror = function() {
-    alert("Couldn't open DB!");
-  }
-  
-  request.onupgradeneeded = function (event) {
-
-    var db = event.target.result;
-
-    var objStore = db.createObjectStore("days", {keyPath: "date"});
-    var attribute = objStore.createIndex("by_hours", "hours", { unique: false });
-};
-  
+      var db = event.target.result;
+      var objStore = db.createObjectStore("days", {keyPath: "date"});
+      var attribute = objStore.createIndex("by_hours", "hours", { unique: false });
+      
+    };
+    
+  });
 }
 
 function getDay(date) {
@@ -62,19 +64,25 @@ function getDay(date) {
 }
 
 function addDay(date, hours) {
+  
+  if (isNaN(hours)) {
+    alert('The amount of hours you put is not a number!');
+  } else {
 
-  let tx = db.transaction(['days'], "readwrite");
-  let txObj = tx.objectStore('days');
+    let tx = db.transaction(['days'], "readwrite");
+    let txObj = tx.objectStore('days');
 
-  console.log(date+", "+hours);
-  let request = txObj.put({date: date, hours: Number(hours)});
+    console.log(date+", "+hours);
+    let request = txObj.put({date: date, hours: Number(hours)});
 
-  request.onsuccess = function() {
+    request.onsuccess = function() {
 
-    console.log("Success!");
+      console.log("Success!");
 
-    // getRecord(store, request.result);
+      // getRecord(store, request.result);
 
+    }
+    
   }
 
 }
@@ -108,30 +116,38 @@ function getAllHours() {
 }
 
 function calculateOvertime() {
-  
-  getAllHours().then(function(dayResponse) {
+  return new Promise(function(resolve, reject) {
     
-    console.log(dayResponse);
+    getAllHours().then(function(dayResponse) {
+      
+      console.log(dayResponse);
+      
+      let totalHours = 0;
+      
+      let firstDate = new Date(dayResponse[0].date);
+      let lastDate;
+      let today = new Date(Date.now());
+      
+      for (let currentDay in dayResponse) {
+        totalHours += dayResponse[currentDay].hours;
+        lastDate = new Date(dayResponse[currentDay].date);
+      }
+      
+      let hoursPerWeek = 11;    
+      // let totalDays = Math.round(Math.abs((firstDate.getTime() - lastDate.getTime())/(24*60*60*1000))+1);
+      let totalDays = Math.round(Math.abs((firstDate.getTime() - today.getTime())/(24*60*60*1000)));
+      // let totalRequiredHours = totalDays*(hoursPerWeek/7);
+      let totalRequiredHours = Math.ceil(totalDays*(hoursPerWeek/7));
+      let overtime = totalHours - totalRequiredHours;
+      
+      console.log('Total Days: ' + totalDays);   
+      console.log('Required Hours: ' + totalRequiredHours);
+      console.log('Actual Hours: ' + totalHours);
+      console.log('Overtime: ' + overtime);
+      
+      resolve(overtime);
+      
+    });
     
-    let totalHours = 0;
-    
-    let firstDate = new Date(dayResponse[0].date);
-    let lastDate;
-    
-    for (let currentDay in dayResponse) {
-      totalHours += dayResponse[currentDay].hours;
-      lastDate = new Date(dayResponse[currentDay].date);
-    }
-    
-    let hoursPerWeek = 11;    
-    let totalDays = Math.round(Math.abs((firstDate.getTime() - lastDate.getTime())/(24*60*60*1000))+1);
-    // let totalRequiredHours = totalDays*(hoursPerWeek/7);
-    let totalRequiredHours = Math.ceil(totalDays*(hoursPerWeek/7));
-    
-    console.log('Total Days: ' + totalDays);   
-    console.log('Required Hours: ' + totalRequiredHours);
-    console.log('Actual Hours: ' + totalHours);
-    
-  })
-  
+  });  
 }
