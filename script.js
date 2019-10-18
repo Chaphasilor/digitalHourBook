701a7ee0579c24211c7dc1e0788076ed3769c221
Chaphasilor <ppp.u@web.de>
2019-10-18 15:19:27 +0200

    Google Cloud first attempt

--- script.js
+++ script.js
@@ -1,69 +1,69 @@
 var globalAttributes = {};
 
-Date.prototype.toDateInputValue = (function() {
-    var local = new Date(this);
-    local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
-    return local.toJSON().slice(0,10);
+Date.prototype.toDateInputValue = (function () {
+  var local = new Date(this);
+  local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
+  return local.toJSON().slice(0, 10);
 });
 
 function newHoursEntry() {
-  
+
   document.querySelector('#hoursEntry').style.height = 'calc( 3 * var(--hourDiffHeight)';
-  
+
 }
 
 function newHolidaysEntry() {
-  
+
   document.querySelector('#holidaysEntry').style.height = 'calc( 3 * var(--hourDiffHeight)';
-  
+
 }
 
 var db;
 
 function initDB() {
-  return new Promise(function(resolve, reject) {
-    
+  return new Promise(function (resolve, reject) {
+
     var request = window.indexedDB.open("hourBook", 4);
-    
-    request.onsuccess = function(event) {
-            
+
+    request.onsuccess = function (event) {
+
       db = event.target.result;
-      
-      db.onerror = function(event) {
+
+      db.onerror = function (event) {
         reject("Database error: " + event.target.errorCode);
       }
-      
+
       resolve("DB opened successfully!");
-      
+
     }
-    
-    request.onerror = function() {
+
+    request.onerror = function () {
       reject("Couldn't open DB!");
     }
-    
+
     request.onupgradeneeded = function (event) {
-  
+
       var db = event.target.result;
-      
+
       try {
-        let days = db.createObjectStore("days", {keyPath: "date"});
-        let hours = days.createIndex("by_hours", "hours", {unique: false});
+        let days = db.createObjectStore("days", { keyPath: "date" });
+        let hours = days.createIndex("by_hours", "hours", { unique: false });
         let firstDay = '2018-10-22'; // The first working day ever
         let firstDayHours = 2 // The amount of hours you've worked on your first day
         // addRecord(firstDay, firstDayHours);
-      } catch(e) {
+      } catch (e) {
         console.log("Object store already exists");
       }
-      
+
       try {
-        let holidays = db.createObjectStore("daysOff", {keyPath: "startDate"});
-        let timeOff = holidays.createIndex("by_timeOff", "timeOff", {unique: false});
-      } catch(e) {
+        let holidays = db.createObjectStore("daysOff", { keyPath: "startDate" });
+        let timeOff = holidays.createIndex("by_timeOff", "timeOff", { unique: false });
+      } catch (e) {
         console.log("Object store already exists");
       }
-      
+
     };
-    
+
   });
 }
 
@@ -73,21 +73,21 @@ function getDay(date) {
   let txObj = tx.objectStore('days');
 
   let request = txObj.get(date);
-  request.onsuccess = function() {
+  request.onsuccess = function () {
     let matching = request.result;
     if (matching !== undefined) {
       // A match was found.
       console.log(matching.date + ", " + matching.hours);
     } else {
       // No match was found.
-       console.log("No matching record!");
+      console.log("No matching record!");
     }
   };
 
 }
 
 function addDay(date, hours) {
-  
+
   if (isNaN(hours)) {
     alert("The amount of hours you put is not a number!");
   } else {
@@ -95,16 +95,16 @@ function addDay(date, hours) {
     let tx = db.transaction(['days'], "readwrite");
     let txObj = tx.objectStore('days');
 
-    console.log(date+", "+hours);
-    let request = txObj.put({date: date, hours: Number(hours)});
+    console.log(date + ", " + hours);
+    let request = txObj.put({ date: date, hours: Number(hours) });
 
-    request.onsuccess = function() {
-      
+    request.onsuccess = function () {
+
       console.log("Success");
       window.location.reload();
-      
+
     }
-    
+
   }
 
 }
@@ -116,130 +116,143 @@ function deleteDay(date) {
 
   let request = txObj.delete(date);
 
-  request.onsuccess = function() {
+  request.onsuccess = function () {
     console.log("Success!");
   }
 
 }
 
 function addHolidays(date, timeOff) {
-  
-    if (isNaN(timeOff)) {
-      alert("The amount of days off you've put is not a number!");
-    } else {
-  
-      let tx = db.transaction(['daysOff'], "readwrite");
-      let txObj = tx.objectStore('daysOff');
-  
-      console.log(date+", "+timeOff);
-      let request = txObj.put({startDate: date, timeOff: Number(timeOff)});
-  
-      request.onsuccess = function() {
-        console.log("Success!");  
-        window.location.reload();
-      }
-      
+
+  if (isNaN(timeOff)) {
+    alert("The amount of days off you've put is not a number!");
+  } else {
+
+    let tx = db.transaction(['daysOff'], "readwrite");
+    let txObj = tx.objectStore('daysOff');
+
+    console.log(date + ", " + timeOff);
+    let request = txObj.put({ startDate: date, timeOff: Number(timeOff) });
+
+    request.onsuccess = function () {
+      console.log("Success!");
+      window.location.reload();
     }
-  
+
+  }
+
 }
 
 function getAllHours() {
-  return new Promise(function(resolve, reject) {
-    
+  return new Promise(function (resolve, reject) {
+
     let transaction = db.transaction(['days'], "readonly");
     let objectStore = transaction.objectStore('days');
-    
+
     let request = objectStore.getAll();
-    
-    request.onsuccess = function() {
+
+    request.onsuccess = function () {
       resolve(request.result);
     }
-  
+
   });
 }
 
 function getDaysOff() {
-  return new Promise(function(resolve, reject) {
-    
+  return new Promise(function (resolve, reject) {
+
     let transaction = db.transaction(['daysOff'], "readonly");
     let objectStore = transaction.objectStore('daysOff');
-    
+
     let request = objectStore.getAll();
-    
-    request.onsuccess = function() {
+
+    request.onsuccess = function () {
       resolve(request.result);
     }
-  
+
   });
 }
 
 function calculateOvertime() {
-  return new Promise(function(resolve, reject) {
-    
-    getAllHours().then(function(daysResponse) {
-      
-      
+  return new Promise(function (resolve, reject) {
+
+    getAllHours().then(function (daysResponse) {
+
+
       console.log(daysResponse);
-      
+
       let totalHours = 0;
-      
+
       let firstDate = new Date(daysResponse[0].date);
       // let lastDate;
       let today = new Date(Date.now());
-      
+
       getDaysOff().then(daysOffResponse => {
-        
+
         console.log(daysOffResponse);
         let countDaysOff = 0;
         for (let day in daysOffResponse) {
           day = daysOffResponse[day];
           let currentDate = new Date(day.startDate);
-          let difference = Math.round((currentDate.getTime() - firstDate.getTime())/(24*60*60*1000));
+          let difference = Math.round((currentDate.getTime() - firstDate.getTime()) / (24 * 60 * 60 * 1000));
           console.log(difference);
           console.log(day, !(difference < 0 && (difference + day.timeOff) < 1));
-          
+
           if (!((difference + day.timeOff) < 1)) {
             if (difference < 0) {
               countDaysOff += difference + day.timeOff; // difference is negative, add only the days that go beyond the first day
             } else {
-              if (!(Math.round((today.getTime() - currentDate.getTime())/(24*60*60*1000))-day.timeOff+1 < 0)) { // holiday is not in the future
-                if (Math.round((today.getTime() - currentDate.getTime() + (day.timeOff-1)*86400000)/(24*60*60*1000)) < 0) { // some holidays in the future
-                  countDaysOff += day.timeOff + Math.round((today.getTime() - currentDate.getTime() + (day.timeOff-1)*86400000)/(24*60*60*1000)); // add remainder of holidays
+              if (!(Math.round((today.getTime() - currentDate.getTime()) / (24 * 60 * 60 * 1000)) - day.timeOff + 1 < 0)) { // holiday is not in the future
+                if (Math.round((today.getTime() - currentDate.getTime() + (day.timeOff - 1) * 86400000) / (24 * 60 * 60 * 1000)) < 0) { // some holidays in the future
+                  countDaysOff += day.timeOff + Math.round((today.getTime() - currentDate.getTime() + (day.timeOff - 1) * 86400000) / (24 * 60 * 60 * 1000)); // add remainder of holidays
                 } else {
                   countDaysOff += day.timeOff
                 }
-              } 
+              }
             }
-          }  
-        }
-        
-        globalAttributes.daysOff = countDaysOff;
-        
-      }).then(_ => {
-        
-          for (let currentDay in daysResponse) {
-            totalHours += daysResponse[currentDay].hours;
-            // lastDate = new Date(daysResponse[currentDay].date);
           }
-          
-          let hoursPerWeek = 7;    
-          // let totalDays = Math.round(Math.abs((firstDate.getTime() - lastDate.getTime())/(24*60*60*1000))+1);
-          console.log(globalAttributes.daysOff);
-          let totalDays = Math.round(Math.abs((firstDate.getTime() - today.getTime())/(24*60*60*1000))) - globalAttributes.daysOff;
-          // let totalRequiredHours = totalDays*(hoursPerWeek/7);
-          let totalRequiredHours = Math.ceil(totalDays/7)*hoursPerWeek;
-          let overtime = totalHours - totalRequiredHours;
-          
-          console.log('Total Days: ' + totalDays);   
-          console.log('Required Hours: ' + totalRequiredHours);
-          console.log('Actual Hours: ' + totalHours);
-          console.log('Overtime: ' + overtime);
-          
-          resolve(overtime);
-        
+        }
+
+        globalAttributes.daysOff = countDaysOff;
+
+      }).then(_ => {
+
+        for (let currentDay in daysResponse) {
+          totalHours += daysResponse[currentDay].hours;
+          // lastDate = new Date(daysResponse[currentDay].date);
+        }
+
+        let hoursPerWeek = 7;
+        // let weeksPerMonth = 4.349; // constant used by GSI
+        // let daysPerMonth = 7*weeksPerMonth;
+        // let hoursPerDay = (hoursPerWeek*weeksPerMonth)/daysPerMonth;
+        // let totalDays = Math.round(Math.abs((firstDate.getTime() - lastDate.getTime())/(24*60*60*1000))+1);
+        console.log(globalAttributes.daysOff);
+        let totalDays = Math.round(Math.abs((firstDate.getTime() - today.getTime()) / (24 * 60 * 60 * 1000))) - globalAttributes.daysOff;
+        // let totalRequiredHours = totalDays*(hoursPerWeek/7);
+        let totalRequiredHours = Math.ceil(totalDays / 7) * hoursPerWeek;
+        let overtime = totalHours - totalRequiredHours;
+
+        console.log('Total Days: ' + totalDays);
+        console.log('Required Hours: ' + totalRequiredHours);
+        console.log('Actual Hours: ' + totalHours);
+        console.log('Overtime: ' + overtime);
+
+        resolve(overtime);
+
       })
-      
+
     });
-    
-  });  
+
+  });
+}
+
+function importHours(hoursAsJson) {
+
+  let hoursAsObject = JSON.parse(hoursAsJson);
+
+  for (const date of hoursAsObject) {
+    addDay(date.date, date.hours[0].hours);
+  }
+
 }
